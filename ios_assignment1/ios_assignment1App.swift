14442efb8c2568cba276952af41d69a4c6bf0d1a
VizNem <thinhtruong3344@gmail.com>
2022-07-21 13:27:55 +0700

    Initial Commit

---
+++ ios_assignment1/ios_assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_assignment1App.swift
+//  ios_assignment1
+//
+//  Created by Thinh, Nguyen Truong on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ios_assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
