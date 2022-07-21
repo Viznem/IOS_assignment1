8d437acbee219775669f7e43bccd42455fecb372
VizNem <thinhtruong3344@gmail.com>
2022-07-21 14:40:36 +0700

    Adding structure and model for this app

---
+++ ios_assignment1/Model/ColorConstants.swift
@@ -0,0 +1,14 @@
+//
+//  ColorConstants.swift
+//  ios_assignment1
+//
+//  Created by Thinh, Nguyen Truong on 21/07/2022.
+//
+
+import Foundation
+import SwiftUI
+
+struct ColorConstants{
+    static let rmitBlue = Color(red: 0.03, green: 0.03, blue: 0.35)
+    static let rmitRed = Color(red: 0.90, green: 0.00, blue: 0.16)
+}
---
+++ ios_assignment1/Model/ModelData.swift
@@ -0,0 +1,12 @@
+//
+//  ModelData.swift
+//  ios_assignment1
+//
+//  Created by Thinh, Nguyen Truong on 21/07/2022.
+//
+
+import Foundation
+
+var hotels: [Hotel] = [
+    Hotel()
+]
